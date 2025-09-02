b59902b7ce4420ccf9ee833fa76ad176e1709a19
Rep-4 <takuom662634@gmail.com>
2025-09-02 19:20:05 +0900

    Update PI-calculator.js

--- PI-calculator.js
+++ PI-calculator.js
@@ -8,15 +8,23 @@ const reset = document.getElementById('reset');
 let num;
 
 function initialize() {
-	array = [6,BigDecimal.from(3),BigDecimal.from(2).mul(BigDecimal.from(3).sqrt(parseInt(precision.value)))];
+	array = [6,3,BigNumber.multiply("2",`${sqrt("3",parseInt(precision.value))}`)];
 	num = 1;
 	[...valuelist].forEach((e)=>{e.remove();});
 }
 
+function sqrt(n,scale=50) {
+	let x = n;
+	for (let i = 0;i < scale;i++){
+		x = BigNumber.divide(`${BigNumber.add(x,`${BigNumber.divide(n,x)}`)}`,"2"); //x/2+n/2x
+	}
+	return `${x}`
+}
+
 function culculate(array){
 	array[0] = 2*array[0];
-	array[2] =  BigDecimal.from(2).mul( array[1].mul(array[2]).div((array[1].add(array[2])),parseInt(precision.value)) );
-	array[1] = (array[1].mul(array[2])).sqrt(parseInt(precision.value));
+	array[2] =  BigNumber.multiply("2",`${BigNumber.divide(`${BigNumber.multiply(`${array[1]}`,`${array[2]}`)}`,`${BigNumber.add(`${array[1]}`,`${array[2]}`)}`)}` );
+	array[1] = sqrt(`${BigNumber.multiply(`${array[1]}`,`${array[2]}`)}`,parseInt(precision.value));
 	return array
 }
 
